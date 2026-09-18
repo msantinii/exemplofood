ac00aa1167abc68872e149604e18e4697965178e
Maria <mclarasantini@gmail.com>
2026-09-18 08:44:18 -0300

    feat:Banco de dados do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-
+import Pedido from './components/Pedido'
 
 const App = () => {
   return (
     <>
-      
+      <Pedido/>
     </>
   )
 }
--- src/components/pedido.jsx
+++ src/components/pedido.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
 
+//array d eobjeto contendo o estado inicial do cardápio
+const cardapio=[
+  {id:1,nome:"Combo-01",preco:25.00,disponivel:true,quantidade:0},
+  {id:2, nome:"Combo-02", preco:40.00, disponivel:true, quantidade:0},
+  {id:3, nome:"Combo-03", preco:65.00, disponivel:true, quantidade:0},
+  {id:4, nome:"Combo-04", preco:25.00, disponivel:false, quantidade:0},
+]
 
 const pedido = () => {
   return (
